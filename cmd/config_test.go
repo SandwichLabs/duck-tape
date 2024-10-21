77cbf9e677d7192a8912f0b45a2fe424d26f2ac2
Zac Orndorff <zac@orndorff.dev>
2024-10-21 08:20:47 -0500

    initial, basic testing. this aint good yet.

--- cmd/config_test.go
+++ cmd/config_test.go
@@ -5,28 +5,27 @@ import (
 	"os"
 	"testing"
 
+	"github.com/SandwichLabs/duck-tape/cmd"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestEnsureConfig(t *testing.T) {
-	vip := viper.New()
-	vip.Set("workspace", "test_workspace")
+	viper.Set("workspace", "test_workspace")
 
-	configDirPath := "./tmp/test_config_dir"
+	configDirPath := cmd.GetConfigPath()
 
-	// Assert that the workspace database is set
-	dbPath := fmt.Sprintf("%s/.dt/dt.db", configDirPath)
-	_, err := os.Stat(dbPath)
-	assert.NoError(t, err)
+	cmd.InitConfig()
 
 	// Assert that the workspace folder is created
 	workspacePath := fmt.Sprintf("%s/test_workspace", configDirPath)
-	_, err = os.Stat(workspacePath)
+	defer os.RemoveAll(workspacePath)
+
+	_, err := os.Stat(workspacePath)
 	assert.NoError(t, err)
 
 	// Assert that the config file is written
-	configFilePath := fmt.Sprintf("%s/.dt/config.yaml", configDirPath)
+	configFilePath := fmt.Sprintf("%s/config.yaml", configDirPath)
 	_, err = os.Stat(configFilePath)
 	assert.NoError(t, err)
 }
